19cbb2864a6e6123fd2efa3421b3e1398c05dbe3
Viacheslav Golyshevskii <python_poseur@MacBook-Air-Viacheslav.local>
2022-12-09 09:06:48 +0400

    signup config

--- photo/forms.py
+++ photo/forms.py
@@ -4,8 +4,17 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
+    """Form for a new user registration"""
+    
     email = forms.EmailField(max_length=255)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 
 @login_required(login_url='/auth/login/')
@@ -7,3 +9,17 @@ def main_page(request):
     """Function for render main page"""
 
     return render(request, 'base.html')
+
+
+def signup_page(request):
+    """Function for sign up process"""
+    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('mainpage')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', context={'form': form})
