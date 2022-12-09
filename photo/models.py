4a3d67cda7567913aec7c03dd1a767862ca338f2
Viacheslav Golyshevskii <python_poseur@MacBook-Air-Viacheslav.local>
2022-12-09 10:15:22 +0400

    photo model

--- photo/models.py
+++ photo/models.py
@@ -3,12 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Photo(models.Model):
-    img = models.ImageField()
+    img = models.ImageField(verbose_name='Photo', upload_to='images/')
     title = models.CharField(verbose_name='Photo Title', max_length=50)
     desc = models.TextField(verbose_name='Photo Descriprion', blank=True, null=True)
+    people = models.CharField(verbose_name='People on photo', max_length=255, blank=True, null=True)
+    loacation = models.CharField(verbose_name='Location of the photo', max_length=255, blank=True, null=True)
+    tags = models.CharField(verbose_name='Photo Search Tags', max_length=100, blank=True, null=True)
     cr_dt = models.DateTimeField(verbose_name='Photo Created Date', auto_now_add=True)
 
-    user = models.ForeignKey(User, verbose_name='By User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='Created By User', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['title']
 
     def __str__(self):
         return self.title
